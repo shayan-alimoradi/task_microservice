ee7bccd5814b17fe0173a8cb033bd617b65ee5ed
unknown <shayan.aimoradii@gmail.com>
2024-06-18 13:48:02 +0330

    fix(project): Add ID field

--- task/models.py
+++ task/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 
 class Project(models.Model):
+    id = models.BigAutoField(primary_key=True, serialize=False)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
         return self.name
