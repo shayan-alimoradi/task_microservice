addc078f9b459ca9c4b691f0fdbfb085c6ec2e53
unknown <shayan.aimoradii@gmail.com>
2024-06-19 19:02:35 +0330

    chore: Remove unused import

--- task/api/views.py
+++ task/api/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from ..models import Task, Comment
+from ..models import Task
 from .serializers import (
     TaskListSerializer,
     TaskRetrieveSerializer,
